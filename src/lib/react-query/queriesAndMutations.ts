773cb70a1818d66fafa0b945092a08321d6d516d
braaidg <braiandenisgomez@gmail.com>
2023-12-19 17:26:52 -0300

    useCreatePost query created with invalidation queries

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,24 +4,42 @@ import {
   useQueryClient,
   useInfiniteQuery,
 } from "@tanstack/react-query";
-import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
-import { INewUser } from "@/types";
-
+import {
+  createPost,
+  createUserAccount,
+  signInAccount,
+  signOutAccount,
+} from "../appwrite/api";
+import { INewPost, INewUser } from "@/types";
+import { QUERY_KEYS } from "./queryKeys";
 
 export const useCreateUserAccount = () => {
   return useMutation({
-    mutationFn: (user: INewUser) => createUserAccount(user)
-  })
-}
+    mutationFn: (user: INewUser) => createUserAccount(user),
+  });
+};
 
 export const useSignInAccount = () => {
   return useMutation({
-    mutationFn: (user: {email: string, password: string}) => signInAccount(user),
+    mutationFn: (user: { email: string; password: string }) =>
+      signInAccount(user),
   });
 };
 
 export const useSignOutAccount = () => {
   return useMutation({
-    mutationFn: signOutAccount
+    mutationFn: signOutAccount,
+  });
+};
+
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (post: INewPost) => createPost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+    },
   });
 };
